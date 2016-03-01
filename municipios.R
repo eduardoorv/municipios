45f84bc2080d3ce6def1bd0c1d895b1f12b291c8
Eduardo <lalo_8a@ciencias.unam.mx>
2016-03-01 11:57:33 -0600

    Se agregó la implementación en shiny

--- municipios.R
+++ municipios.R
@@ -20,19 +20,52 @@ for (i in 10:32) {
   x[i]=length(mun[mun$CVE_ENT==i,])
 }
 
-x<-order(x)
+y<-sort.list(sort.list(x))
 
 
-b<-colorRampPalette(c("white","red"))(32)[x]
+b<-colorRampPalette(c("white","red"))(32)[y]
 
 m <- leaflet() %>%
   addTiles() %>%  
-  addPolygons(data=mun[mun$CVE_ENT=="20",],stroke = F,popup=dest$NOM_ENT) %>% 
+  addPolygons(data=mun[mun$CVE_ENT=="20",],stroke = T,opacity = .2,popup=dest$NOM_ENT) %>% 
   addProviderTiles("CartoDB.Positron")
 m 
 
 m <- leaflet() %>%
   addTiles() %>%  
   addPolygons(data=dest,color = b,stroke = F,popup=dest$NOM_ENT) %>% 
-  addProviderTiles("CartoDB.Positron")
+  addProviderTiles("CartoDB.Positron") %>%
+  addLegend(pal = colorNumeric(palette = colorRampPalette(c("white","red"))(32),domain = 1:32),values =1:32,title = "Cantidad de Municipios")
+
 m 
+
+#Implementado en shiny
+
+ui <- fluidPage(
+  leafletOutput("mymap"),
+  verbatimTextOutput("stats")
+)
+
+server <- function(input, output, session) {
+  
+  nom<-data.frame(dest$NOM_ENT)
+  numeroMunicipios<-cbind(nom,x,y)
+  names(numeroMunicipios)<-c("Estado","Número de Municipios","Posición")
+  
+  output$stats <- renderPrint({
+    numeroMunicipios
+    
+  })
+  
+  output$mymap<-renderLeaflet({
+      leaflet() %>%
+      addTiles() %>%
+      addPolygons(data=dest,color = b,stroke = F,popup=dest$NOM_ENT) %>%
+      addProviderTiles("CartoDB.Positron") %>%
+      addLegend(pal = colorNumeric(palette = colorRampPalette(c("red","white"))(32),domain = 1:32),values =1:32,title = "Cantidad de Municipios")
+  })
+  
+}
+
+shinyApp(ui, server)
+
